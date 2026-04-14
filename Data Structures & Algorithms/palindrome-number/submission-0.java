164792af3cf175a50a0f7098e963badd89028ed0
Badri Narayanan B R <88156848+badri-2005@users.noreply.github.com>
2026-04-14 13:42:26 +0530

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+        {
+            return false;
+        }
+        int t = x;
+        int rev = 0;
+        while(x>0)
+        {
+            int rem = x%10;
+            rev = rev*10 + rem;
+            x = x / 10;
+        }
+        if(rev==t)
+        return true;
+
+        return false;
+    }
+}
